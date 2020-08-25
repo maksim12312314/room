88a256243ab72d9345e446194a7d53b0f26e78b3
maksim12312314 <maksim.saryev@mail.ru>
2020-08-25 18:40:43 +0300

    room

---
+++ app/src/main/java/com/example/room/main_recycler_view.kt
@@ -0,0 +1,12 @@
+package com.example.room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class main_recycler_view : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_recycler_view)
+    }
+}
